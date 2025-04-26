703ad2b979b16662b182379b45873d84b54e51bc
Thegodlessman <luischang16@hotmail.com>
2025-04-26 12:34:21 -0400

    FEAT: Se ha creado los controladores para la obtencion de datos de ubicacion

--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
-import { getRoleId, selectRole } from "../controllers/profile.controller.js";
-
+import { getRoleId, selectRole, getInstiByParish, getCountries, getStatesByCountry, getMunicipalitiesByState, getParishesByMunicipality } from "../controllers/profile.controller.js";
 
 const profileRouter = Router();
 
 profileRouter.patch('/profile/role/:id', selectRole);
-
 profileRouter.get('/profile/get/role/:rol_name', getRoleId);
 
+profileRouter.get('/profile/get/countries', getCountries)
+profileRouter.get('/profile/get/stateByCountries/:id_country', getStatesByCountry)
+profileRouter.get('/profile/get/munByStates/:id_state', getMunicipalitiesByState)
+profileRouter.get('/profile/get/parishesByMunicipality/:id_mun', getParishesByMunicipality)
+profileRouter.get('/profile/get/institutionsByParish/:id_parish', getInstiByParish)
 
 export default profileRouter; 
